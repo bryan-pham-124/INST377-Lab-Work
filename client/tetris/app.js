d040fa1da3200a0893ead40ced7a5477e460e035
bryan-pham-124 <72675857+bryan-pham-124@users.noreply.github.com>
2022-02-12 14:34:29 -0500

    Tetris - initial commit
    This is the initial commit of tetris. In this commit, I define the basic tetris shapes that will be used in the program.

---
+++ client/tetris/app.js
@@ -0,0 +1,54 @@
+/*
+const lTetromino = 'firstShape';
+const zTetromino = 'secondShape';
+const oTetromino = 'thirdShape';
+const iTetromino = 'fourthShape';
+const tTetromino = 'fifthShape';
+
+//const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino];
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+
+  // The Tetrominoes 
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ];
+  
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ];
+  
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
+  
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+  
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino];
+
+});
