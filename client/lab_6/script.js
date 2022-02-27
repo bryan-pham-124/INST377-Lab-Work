1410a9b372b27245cc2fb88c121f82012c9a7dbe
bryan-pham-124 <painfuljet8@gmail.com>
2022-02-27 15:58:56 -0500

    Lab 6 Code
    Finished 6 code

---
+++ client/lab_6/script.js
@@ -0,0 +1,46 @@
+
+ 
+let listContainer = document.querySelector('#resto-list')
+
+ 
+function dataHandler(inputArray, replaceContent){
+  
+  let restaurantList = Array.from(new Array(15), () => inputArray[Math.floor(Math.random() * 1000)])
+
+  listContainer.textContent = '';
+
+  //code cited from https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
+  restaurantList = restaurantList.map(restr => restr).filter((restrName, index, self) => self.indexOf(restrName) === index)
+
+  restaurantList.map((restr) => listContainer.innerHTML += `<li>${restr.name}</li>`)
+
+}
+ 
+async function mainEvent() { // the async keyword means we can make API requests
+  
+    const form = document.querySelector('.form');
+    const submitButton = document.querySelector('.form-button');
+    submitButton.style.setProperty('--display-value', 'none')
+
+    
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+    console.log('response received')
+    dataHandler(arrayFromJson)
+
+    if(arrayFromJson.length > 0){
+
+      submitButton.style.setProperty('--display-value', 'block')
+      form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+        submitEvent.preventDefault(); // This prevents your page from refreshing!
+        console.log('form submission'); // this is substituting for a "breakpoint"
+        dataHandler(arrayFromJson)
+      });
+      
+    }
+  
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  
