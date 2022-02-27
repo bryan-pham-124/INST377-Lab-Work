6691d69562dc9b024bc88f34fded8760a6d4c27a
bryan-pham-124 <painfuljet8@gmail.com>
2022-02-27 16:08:07 -0500

    Lab 6 Final Comments
    Added some new comments to code

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -5,13 +5,17 @@ let listContainer = document.querySelector('#resto-list')
  
 function dataHandler(inputArray){
   
+  //based on stack overflow link that was provided in lab 6 instructions
   let restaurantList = Array.from(new Array(15), () => inputArray[Math.floor(Math.random() * 1000)])
 
+  //clear ul contents
   listContainer.textContent = '';
 
   //code cited from https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
+  //make unique list entries
   restaurantList = restaurantList.map(restr => restr).filter((restrName, index, self) => self.indexOf(restrName) === index)
 
+  //add entries to webpage
   restaurantList.map((restr) => listContainer.innerHTML += `<li>${restr.name}</li>`)
 
 }
@@ -22,7 +26,6 @@ async function mainEvent() { // the async keyword means we can make API requests
     const submitButton = document.querySelector('.form-button');
     submitButton.style.setProperty('--display-value', 'none')
 
-    
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.log('response received')
